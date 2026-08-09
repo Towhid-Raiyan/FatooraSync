be01a56568c53fe1c521114a6f189be2d74a9242
Towhid-Raiyan <towhid.raiyan.com>
2026-08-09 21:06:25 +0600

    Configure Turbopack to use worktree root directory

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.join(__dirname),
+  },
 };
 
 export default withSentryConfig(nextConfig, { silent: true });
